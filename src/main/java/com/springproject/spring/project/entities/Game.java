11299545b1f68160236a90f3520f533d2f4800b3
Guihasilva <54690403+Guihasilva@users.noreply.github.com>
2023-07-03 13:52:25 -0300

    Seed Created

--- src/main/java/com/springproject/spring/project/entities/Game.java
+++ src/main/java/com/springproject/spring/project/entities/Game.java
@@ -15,7 +15,9 @@ public class Game {
     private String platforms;
     private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game () {}
